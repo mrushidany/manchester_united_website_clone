78d22819819d0044025853ad05c0a478978b64a5
Dany Mrushi <mrushidany@gmail.com>
2023-08-01 18:01:04 +0300

    Refractored the code

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className=''>{children}</body>
+      <body className='w-full min-h-full relative overflow-x-visible'>{children}</body>
     </html>
   )
 }
